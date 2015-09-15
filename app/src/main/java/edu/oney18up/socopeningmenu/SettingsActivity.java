33d2459da476ebae2d3638790e4351772a1c6500
oney18 <oney18@up.edu>
2015-09-15 15:42:24 -0700

    new menu

--- app/src/main/java/edu/oney18up/socopeningmenu/SettingsActivity.java
+++ app/src/main/java/edu/oney18up/socopeningmenu/SettingsActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class SettingsActivity extends ActionBarActivity {
@@ -12,6 +13,14 @@ public class SettingsActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+    }
+
+    protected void onDestroy(){
+        //add code to save settings/apply it to next game
+    }
+
+    public void closeSettings(View view) {
+        finish();
     }
 
     @Override
--- app/src/main/java/edu/oney18up/socopeningmenu/newGameActivity.java
+++ app/src/main/java/edu/oney18up/socopeningmenu/newGameActivity.java
@@ -1,33 +1,24 @@
 package edu.oney18up.socopeningmenu;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 
 
-public class OpeningMenu extends Activity {
+public class newGameActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_opening_menu);
+        setContentView(R.layout.activity_new_game);
     }
-    public void openSettings(View view) {
-        Intent intent = new Intent(this, SettingsActivity.class);
-        startActivity(intent);
-    }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_opening_menu, menu);
+        getMenuInflater().inflate(R.menu.menu_new_game, menu);
         return true;
     }
 
